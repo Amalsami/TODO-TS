3e78ee536ecaccc0713129a227dc483037165d79
amal sami <amalsamii13@gmail.com>
2023-09-19 23:51:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./Todo";
+import Todoo from "./Todoo.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Todo></Todo> */}
+      <Todoo></Todoo>
+    </div>
+  );
+}
+
+export default App;
